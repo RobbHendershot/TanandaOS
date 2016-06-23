3de86888bed663b69212d8c5a18735c5287995d6
Robb Hendershot <robb.hendershot@gmail.com>
2016-06-23 15:42:43 -0600

    Changes to be committed: modified:   README.rst modified:   setup.py modified:   tananda_os_builder.py

--- README.rst
+++ README.rst
@@ -2,14 +2,14 @@
 tananda_os_builder
 ===============================
 
-.. image:: https://badge.fury.io/py/tananda_os_builder.png
-    :target: http://badge.fury.io/py/tananda_os_builder
+.. .. image:: https://badge.fury.io/py/tananda_os_builder.png
+..     :target: http://badge.fury.io/py/tananda_os_builder
 
-.. image:: https://travis-ci.org/RobbHendershot/tananda_os_builder.png?branch=master
-        :target: https://travis-ci.org/RobbHendershot/tananda_os_builder
+.. .. image:: https://travis-ci.org/RobbHendershot/tananda_os_builder.png?branch=master
+..         :target: https://travis-ci.org/RobbHendershot/tananda_os_builder
 
-.. image:: https://pypip.in/d/tananda_os_builder/badge.png
-        :target: https://crate.io/packages/tananda_os_builder?version=latest
+.. .. image:: https://pypip.in/d/tananda_os_builder/badge.png
+..         :target: https://crate.io/packages/tananda_os_builder?version=latest
 
 
 Builds the "Tananda" spin I created for makers and hackers.
--- tananda_os_builder.py
+++ tananda_os_builder.py
@@ -3,33 +3,71 @@
 '''tananda_os_builder
 
 Usage:
-  tananda_os_builder ship new <name>...
-  tananda_os_builder ship <name> move <x> <y> [--speed=<kn>]
-  tananda_os_builder ship shoot <x> <y>
-  tananda_os_builder mine (set|remove) <x> <y> [--moored|--drifting]
+  tananda_os_builder create <fedora_version>
   tananda_os_builder -h | --help
-  tananda_os_builder --version
+  tananda_os_builder -v | --version
 
 Options:
   -h --help     Show this screen.
-  --version     Show version.
-  --speed=<kn>  Speed in knots [default: 10].
-  --moored      Moored (anchored) mine.
-  --drifting    Drifting mine.
+  -v --version  Show version.
+
 '''
 
 from __future__ import unicode_literals, print_function
 from docopt import docopt
+from blessed import Terminal
+from sh import ls, git
+
 
 __version__ = "0.1.0"
 __author__ = "Robb Hendershot"
 __license__ = "MIT"
 
 
-def main():
-    '''Main entry point for the tananda_os_builder CLI.'''
-    args = docopt(__doc__, version=__version__)
-    print(args)
+def main(docopt_args):
+
+	if docopt_args["create"]:
+		create(docopt_args["<fedora_version>"])
+
+
+def create(version_number):
+	heading1("Creating new version based on Fedora " + version_number + "\n") # Switch to master branch.
+
+	# Update git and create new version.
+	heading2("Updating master branch.")
+	print(git.checkout("master"))
+	print(git.pull()) # Bring branch current. 
+
+	heading2("Creating new branch")
+	print(git.checkout("-b" + version_number)) # Create working branch.
+
+	# Get kickstart files.
+	# Meld kickstart files together.
+
+
+def delete(version_number):
+	heading1("Deleting version " + version_number + "\n")
+	print(git.checkout("master"))
+	print(git.branch("-D", version_number))
+
+
+def heading1(text):
+
+	term = Terminal()
+
+	text.upper()
+
+	print(term.bold + ("=" * 70))
+	print(term.bold + text.upper() + term.normal)
+
+
+def heading2(text):
+
+	term = Terminal()
+
+	print(term.bold_underline + text + term.normal)
+
 
 if __name__ == '__main__':
-    main()
+  args = docopt(__doc__, version=__version__)
+  main(args)
